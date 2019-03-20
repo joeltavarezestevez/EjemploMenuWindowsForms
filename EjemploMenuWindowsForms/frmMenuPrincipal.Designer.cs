344364f2f26cbe1c0a9f86e008af9372135514f5
Joel Tavarez <joeltavarezestevez@gmail.com>
2019-03-20 16:33:34 -0400

    First Commit

---
+++ EjemploMenuWindowsForms/frmMenuPrincipal.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace EjemploMenuWindowsForms
+{
+    partial class frmMenuPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formulario1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formulario2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opcionesToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(569, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formulario1ToolStripMenuItem,
+            this.formulario2ToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // formulario1ToolStripMenuItem
+            // 
+            this.formulario1ToolStripMenuItem.Name = "formulario1ToolStripMenuItem";
+            this.formulario1ToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.formulario1ToolStripMenuItem.Text = "Registro de Clientes";
+            this.formulario1ToolStripMenuItem.Click += new System.EventHandler(this.formulario1ToolStripMenuItem_Click);
+            // 
+            // formulario2ToolStripMenuItem
+            // 
+            this.formulario2ToolStripMenuItem.Name = "formulario2ToolStripMenuItem";
+            this.formulario2ToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.formulario2ToolStripMenuItem.Text = "Registro de Productos";
+            this.formulario2ToolStripMenuItem.Click += new System.EventHandler(this.formulario2ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 290);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Menu Principal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formulario1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formulario2ToolStripMenuItem;
+    }
+}
+
---
+++ EjemploMenuWindowsForms/frmMenuPrincipal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EjemploMenuWindowsForms
+{
+    public partial class frmMenuPrincipal : Form
+    {
+        public frmMenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void formulario1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Abrir formulario como modal
+            Form regclientes = new frmRegistroClientes();
+            regclientes.ShowDialog();
+        }
+
+        private void formulario2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Abrir formulario normal
+            Form regproductos = new frmRegistroProductos();
+            regproductos.Show();
+        }
+
+
+    }
+}
